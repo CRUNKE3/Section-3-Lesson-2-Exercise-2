ab70cff33537b3ed12e2f9c9bc239e6fad2ce0f8
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2020-02-20 12:34:27 -0500

    double is important for decimals

--- Chickens02.java
+++ Chickens02.java
@@ -4,7 +4,14 @@ package chickens02;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put yout code here
-
+        int monday = 100;
+        int tuesday = 121;
+        int wednesday = 117;
+        double dailyAverage = (monday+tuesday+wednesday)/3.0;
+        double monthlyAverage = dailyAverage*30;
+        double sellPrice = 0.18;
+        double monthlyProfit = monthlyAverage*sellPrice;
+        
         System.out.println("Daily Average:   " +dailyAverage);
         System.out.println("Monthly Average: " +monthlyAverage);
         System.out.println("Monthly Profit:  $" +monthlyProfit);
